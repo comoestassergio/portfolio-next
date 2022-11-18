7e83c1349eab1e9231aef76c33d9fe4d4e7d93cb
sergio <dirtycrew666@gmail.com>
2022-11-18 17:00:21 +0300

    Fix text gradient

--- pages/contact.js
+++ pages/contact.js
@@ -26,7 +26,7 @@ export default function Contact(){
                 exit={{y: '100%'}} 
                 className="absolute h-full w-full flex items-center justify-center gap-5 p-5 overflow-hidden sm:ml-8 md:max-w-lg md:mx-auto md:items-center"
             >
-                <h1 className="text-6xl text-left font-bold text-gray-800 uppercase dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400 dark:text-transparent">Hit<br/>me<br/>up</h1>
+                <h1 className="text-6xl text-left font-bold text-gray-800 uppercase dark:bg-clip-text dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400 dark:text-transparent">Hit<br/>me<br/>up</h1>
                 <ul className="grid grid-cols-2 gap-3">
                     <Link href={links.telegram} target='_blank'>
                         <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize dark:hover:bg-gradient-to-r dark:hover:from-fuchsia-400 dark:hover:to-purple-400 dark:hover:border-purple-400">
