ddcddaf17ec750afc7c436c387750a4fda45ea1b
sergio <dirtycrew666@gmail.com>
2022-11-18 16:58:24 +0300

    Change layout and add gradient

--- pages/contact.js
+++ pages/contact.js
@@ -24,22 +24,22 @@ export default function Contact(){
                 animate={{opacity: 1, y: '0%'}} 
                 transition={{ease: "easeOut"}} 
                 exit={{y: '100%'}} 
-                className="absolute h-full w-full flex flex-col justify-center p-5 overflow-hidden sm:ml-8 md:max-w-lg md:mx-auto md:items-center"
+                className="absolute h-full w-full flex items-center justify-center gap-5 p-5 overflow-hidden sm:ml-8 md:max-w-lg md:mx-auto md:items-center"
             >
-                <h1 className="text-5xl mb-5 text-center font-bold text-gray-800 dark:text-gray-300">Hit me up</h1>
-                <ul className="grid grid-cols-2 py-5 gap-3">
+                <h1 className="text-6xl text-left font-bold text-gray-800 uppercase dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400 dark:text-transparent">Hit<br/>me<br/>up</h1>
+                <ul className="grid grid-cols-2 gap-3">
                     <Link href={links.telegram} target='_blank'>
-                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize">
+                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize dark:hover:bg-gradient-to-r dark:hover:from-fuchsia-400 dark:hover:to-purple-400 dark:hover:border-purple-400">
                             <BsTelegram className="text-2xl" /> Telegram
                         </li>
                     </Link>
                     <Link href={links.github} target='_blank'>
-                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize">
+                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize dark:hover:bg-gradient-to-r dark:hover:from-fuchsia-400 dark:hover:to-purple-400 dark:hover:border-purple-400">
                             <BsGithub className="text-2xl" /> GitHub
                         </li>
                     </Link>
-                    <Link href={'/contact/resume'}>
-                        <li className="btn h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize">
+                    <Link className=" col-span-2" href={'/contact/resume'}>
+                        <li className="btn btn-secondary btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize">
                             <BiFileBlank className="text-2xl" /> resume
                         </li>
                     </Link>
