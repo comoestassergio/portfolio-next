21de68fc4109cb9d25a2fd6dd92647bf4d31121e
sergio <dirtycrew666@gmail.com>
2022-11-18 17:02:10 +0300

    Change color for dark mode

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
         <>
             <header className=" absolute top-0 flex justify-between items-center p-4 md:px-10 lg:px-20 z-10">
                 <h1 className="font-bold text-xl">
-                    <Link href="/">comoestassergio</Link>
+                    <Link href="/" className="dark:text-gray-400">comoestassergio</Link>
                 </h1>
             </header>
             <main>{children}</main>
