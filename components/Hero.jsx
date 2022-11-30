27fc1855b464ba0adacbf021e24534cd6b310951
sergio <dirtycrew666@gmail.com>
2022-11-30 21:57:18 +0300

    Tweak styles for larger screens

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,22 +8,22 @@ const Hero = ({ firstName, lastName, title, text }) => {
         <m.div initial={{opacity: 0, scale: 0.8}} 
                 animate={{opacity: 1, scale: 1}} 
                 exit={{opacity: 0, scale: 0}}
-                className="absolute h-full w-full flex flex-col justify-center pl-4 md:max-w-lg md:ml-32 lg:ml-80"
+                className="absolute h-full w-full flex flex-col justify-center pl-4 md:max-w-lg md:ml-16 lg:max-w-xl"
             >
-                <h1 className="text-5xl max-w-sm md:max-w-lg">
+                <h1 className="text-5xl max-w-sm md:max-w-lg md:text-8xl lg:text-9xl lg:uppercase">
                     <span className="text-gray-600 overflow-hidden dark:text-gray-400">{firstName}</span> <span className="font-bold dark:bg-clip-text dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400 dark:text-transparent">{lastName}</span>
                 </h1>
-                <p className="capitalize text-2xl font-light text-gray-500 mt-5">{title}</p>
-                <p className="mt-10 leading-8 max-w-xs text-gray-800 text-lg dark:text-gray-300 md:max-w-sm">
+                <p className="capitalize text-2xl font-light text-gray-500 mt-5 md:text-3xl">{title}</p>
+                <p className="mt-10 leading-8 max-w-xs text-gray-800 text-lg dark:text-gray-300 md:max-w-sm md:text-xl lg:max-w-2xl">
                     {text} 
                 </p>
                 <div className="flex items-center mt-8 gap-2">
 
                     <Link href={'/projects'}>
-                        <button className="btn btn-secondary rounded-lg shadow-lg shadow-secondary/50 uppercase dark:shadow-none dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400">See my projects</button>
+                        <button className="btn btn-secondary rounded-lg shadow-lg shadow-secondary/50 uppercase dark:shadow-none dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400 lg:w-80">See my projects</button>
                     </Link>
                     <Link href={'/contact'}>
-                        <button className="btn btn-ghost rounded-lg uppercase">Contact</button>
+                        <button className="btn btn-ghost rounded-lg uppercase lg:w-48">Contact</button>
                     </Link>
                 </div>
         </m.div>
--- pages/contact.js
+++ pages/contact.js
@@ -24,23 +24,23 @@ export default function Contact(){
                 animate={{opacity: 1, y: '0%'}} 
                 transition={{ease: "easeOut"}} 
                 exit={{y: '100%'}} 
-                className="absolute h-full w-full flex items-center justify-center gap-5 p-5 overflow-hidden sm:ml-8 md:max-w-lg md:mx-auto md:items-center"
+                className="absolute h-full w-full flex items-center justify-center gap-5 p-5 overflow-hidden sm:ml-8 md:max-w-lg md:mx-auto md:items-center md:ml-16 lg:max-w-fit"
             >
-                <h1 className="text-6xl text-left font-bold text-gray-800 uppercase dark:bg-clip-text dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400 dark:text-transparent">Hit<br/>me<br/>up</h1>
+                <h1 className="text-6xl text-left font-bold text-gray-800 uppercase dark:bg-clip-text dark:bg-gradient-to-r dark:from-fuchsia-400 dark:to-purple-400 dark:text-transparent lg:text-9xl">Hit<br/>me<br/>up</h1>
                 <ul className="grid grid-cols-2 gap-3">
                     <Link href={links.telegram} target='_blank'>
-                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize dark:hover:bg-gradient-to-r dark:hover:from-fuchsia-400 dark:hover:to-purple-400 dark:hover:border-purple-400">
-                            <BsTelegram className="text-2xl" /> Telegram
+                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize dark:hover:bg-gradient-to-r dark:hover:from-fuchsia-400 dark:hover:to-purple-400 dark:hover:border-purple-400 lg:h-48 lg:w-48">
+                            <BsTelegram className="text-2xl lg:text-4xl" /> Telegram
                         </li>
                     </Link>
                     <Link href={links.github} target='_blank'>
-                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize dark:hover:bg-gradient-to-r dark:hover:from-fuchsia-400 dark:hover:to-purple-400 dark:hover:border-purple-400">
-                            <BsGithub className="text-2xl" /> GitHub
+                        <li className="btn btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize dark:hover:bg-gradient-to-r dark:hover:from-fuchsia-400 dark:hover:to-purple-400 dark:hover:border-purple-400 lg:h-48 lg:w-48">
+                            <BsGithub className="text-2xl lg:text-4xl" /> GitHub
                         </li>
                     </Link>
                     <Link className=" col-span-2" href={'/contact/resume'}>
-                        <li className="btn btn-secondary btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize">
-                            <BiFileBlank className="text-2xl" /> resume
+                        <li className="btn btn-secondary btn-outline h-20 rounded-lg flex flex-col gap-1 items-center text-lg capitalize lg:h-40">
+                            <BiFileBlank className="text-2xl lg:text-4xl" /> resume
                         </li>
                     </Link>
                 </ul>
