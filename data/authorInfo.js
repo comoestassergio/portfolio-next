fde2ae244cd35bd3abae98d30705f2a73bdfa2d5
comoestassergio <dirtycrew666@gmail.com>
2023-01-14 15:33:07 +0300

    Add two config files to keep track of data

---
+++ data/authorInfo.js
@@ -0,0 +1,8 @@
+const authorInfo = {
+    firstName: 'Sergey',
+    lastName: 'Chernyavskiy',
+    title: 'Front-end Developer',
+    description: "I'm a web dev based in Russia. Wanna make a cool web app or create a stunning design? Hit me up, I'd love to connect."
+}
+
+export default authorInfo
---
+++ data/siteMetadata.js
@@ -0,0 +1,8 @@
+const siteMetadata = {
+    resumeLink: 'https://drive.google.com/file/d/1mypqoDFwGTvgTOb3A3RsSN0kj7V1XPok/view?usp=share_link',
+    name: 'comoestassergio',
+    github: `https://github.com/comoestassergio`, 
+    telegram:'https://t.me/comoestassergio',
+}
+
+export default siteMetadata
