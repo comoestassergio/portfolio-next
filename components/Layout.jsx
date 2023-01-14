1d1a08f9eebf253b5f7b238aea7889c8701d4ca6
comoestassergio <dirtycrew666@gmail.com>
2023-01-14 15:35:05 +0300

    Change link text to metadata import

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import siteMetadata from "../data/siteMetadata";
 
 const Layout = ({ children }) => {
     return (
         <>
             <header className=" absolute top-0 flex justify-between items-center p-4 md:px-10 lg:px-20 z-10">
                 <h1 className="font-bold text-xl">
-                    <Link href="/" className="dark:text-gray-400">comoestassergio</Link>
+                    <Link href="/" className="dark:text-gray-400">{siteMetadata.name}</Link>
                 </h1>
             </header>
             <main>{children}</main>
