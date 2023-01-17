5ab67c60db09e6c4444c4ad6452b76b87da75762
comoestassergio <dirtycrew666@gmail.com>
2023-01-17 11:08:30 +0300

    Rework project modal and project card

--- components/Card.tsx
+++ components/Card.tsx
@@ -17,7 +17,7 @@ export default function Card ({ id, name, homepage, topics, description, html_ur
     const imgPath = `/images/${id}.png`
 
     const handleClick = () => {
-        router.push(`/projects/?project=${id}`)
+        router.push(`/projects/?project=${id}`, undefined, {shallow: true})
     }
 
     return (
@@ -27,12 +27,12 @@ export default function Card ({ id, name, homepage, topics, description, html_ur
             transition={{ease: "easeOut"}} 
             exit={{ scale: 0, opacity: 0 }}  
             key={id} 
-            className={`card w-full max-w-full shadow-xl border-[0.5px] border-gray-200/50 md:card-side md:max-w-2xl dark:border-none dark:rounded-lg lg:max-w-5xl relative`}>
+            className={`card w-full max-w-full shadow-xl border-[0.5px] border-gray-200/50 md:card-side dark:border-none dark:rounded-lg lg:max-w-5xl relative`}>
             <div className="relative w-auto h-80 md:h-80 md:w-80 lg:h-auto lg:w-3/4">
                 <Image className="object-cover" fill={true} src={imgPath} alt={name}/>
             </div>
             <div className="card-body gap-4 dark:bg-stone-900 md:w-[50%] lg:h-[50vh]">
-                <label onClick={handleClick} htmlFor="project-modal" className="card-title capitalize hover:underline dark:bg-clip-text dark:animate-gradient-glow dark:bg-gradient-to-r dark:from-pink-400 dark:to-purple-400 dark:text-transparent">{cleanName}</label>
+                <h2  className="card-title capitalize dark:bg-clip-text dark:animate-gradient-glow dark:bg-gradient-to-r dark:from-pink-400 dark:to-purple-400 dark:text-transparent">{cleanName}</h2>
                 <p className="dark:text-stone-400">{description}</p>
                 {topics &&
                     <ul className="flex flex-wrap min-w-xs gap-2 mb-3">
@@ -43,12 +43,7 @@ export default function Card ({ id, name, homepage, topics, description, html_ur
                     </ul>
                 }
                 <div className="card-actions justify-end">
-                    <Link href={`${homepage}`} target='_blank'>
-                        <button className="btn btn-secondary rounded-lg shadow-lg shadow-secondary/50 dark:rounded-lg dark:uppercase dark:animate-gradient-glow dark:bg-gradient-to-r dark:from-pink-400 dark:to-purple-400 dark:text-purple-200">Live Preview</button>
-                    </Link>
-                    <Link href={`${html_url}`} target='_blank'>
-                        <button className="btn btn-outline dark:rounded-lg dark:btn-ghost dark:uppercase">View Code</button>
-                    </Link>
+                    <label tabIndex={0} onClick={handleClick} htmlFor="project-modal" className="btn btn-secondary rounded-lg shadow-lg shadow-secondary/50 dark:rounded-lg dark:uppercase dark:animate-gradient-glow dark:bg-gradient-to-r dark:from-pink-400 dark:to-purple-400 dark:text-purple-200">Open Info</label>
                 </div>
             </div>
         </m.li>
--- components/ProjectPopUp.tsx
+++ components/ProjectPopUp.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { Project, ReadMe } from "../pages/projects";
 import ReactMarkdown from "react-markdown";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState, useRef } from "react";
+import Link from "next/link";
 
 
 type ProjectPopUpProps = {
@@ -23,36 +23,41 @@ export default function ProjectPopUp ({ projects, readMes }: ProjectPopUpProps) 
 
     const currentReadme = readMes.find(el => el.id.toString() === project)?.markdown
 
+
     return (
         <>
             <input type="checkbox" id="project-modal" className="modal-toggle" />
-                <label htmlFor="project-modal" className="modal cursor-pointer">
-                    <label className="modal-box relative" htmlFor="">
-                        <div className="relative w-auto h-96 mb-5 md:h-80 md:w-80 lg:h-auto lg:w-3/4">
-                            <Image className="object-cover rounded-xl" fill={true} src={`/images/${currentProject?.id}.png`} alt={currentProject?.name}/>
-                        </div>
-                        {/* <h3 className="text-3xl font-bold first-letter:uppercase">{currentProject?.name}</h3>
-                        <p className="py-4 text-lg mb-3">{currentProject?.description}</p> */}
-                        {/* {currentProject?.topics &&
-                            <ul className="flex flex-wrap min-w-xs gap-2 mb-3">
-                                {currentProject?.topics?.map((el, index) => (
-                                    <li key={index} className="px-4 py-2 bg-base-300 rounded-lg dark:rounded-lg dark:text-stone-300 dark:bg-stone-800">{el}</li>
-                                ))
-                                }
-                            </ul>
-                        } */}
-                        {currentReadme && 
-                            <ReactMarkdown 
-                                children={currentReadme} 
-                                components={{
-                                    h1: ({node, ...props}) => <h1 className="text-2xl font-semibold" {...props} />,
-                                    h2: ({node, ...props}) => <h2 className="text-xl font-semibold" {...props} />,
-                                    h3: ({node, ...props}) => <h2 className="text-lg font-semibold" {...props} />,
-                                    ul: ({node, ...props}) => <ul className=" opacity-70 list-disc ml-6 text-lg break-words flex flex-col gap-3" {...props} />,
-                                    p: ({node, ...props}) => <p className="text-lg opacity-75 leading-[200%]" {...props} />
-                                }}
-                            />
+                <label  htmlFor="project-modal" className="modal cursor-pointer">
+                    <label className="modal-box relative overflow-y-auto md:max-w-3xl lg:flex lg:items-center lg:gap-3 lg:max-w-5xl" htmlFor="">
+                        {project &&
+                            <div className="relative w-auto h-80 mb-5 md:h-96 lg:w-[50vw] lg:h-[50vh] lg:max-h-sm">
+                                <Image className="object-cover rounded-xl" fill={true} src={`/images/${currentProject?.id}.png`} alt={currentProject?.name}/>
+                            </div>
                         }
+                        {currentReadme && 
+                            <div className="flex flex-col max-w-full lg:max-w-md">
+                                <ReactMarkdown 
+                                    children={currentReadme} 
+                                    components={{
+                                        h1: ({node, ...props}) => <h1 className="text-2xl font-semibold mb-3" {...props} />,
+                                        h2: ({node, ...props}) => <h2 className="text-xl font-semibold mb-2" {...props} />,
+                                        h3: ({node, ...props}) => <h2 className="text-lg font-semibold mb-1" {...props} />,
+                                        ul: ({node, ...props}) => <ul className=" opacity-70 list-disc ml-6 text-lg break-words flex flex-col gap-3" {...props} />,
+                                        p: ({node, ...props}) => <p className="text-lg opacity-75 leading-[200%] my-3" {...props} />
+                                    }}
+                                />
+                                <div className="flex justify-end gap-3 mt-5 lg:mt-10">
+                                    <Link href={`${currentProject.homepage}`} target='_blank' className="btn btn-secondary rounded-lg shadow-lg shadow-secondary/50 dark:rounded-lg dark:uppercase dark:animate-gradient-glow dark:bg-gradient-to-r dark:from-pink-400 dark:to-purple-400 dark:text-purple-200">
+                                        Live Preview
+                                    </Link>
+                                    <Link href={`${currentProject.html_url}`} target='_blank' className="btn btn-outline dark:rounded-lg dark:btn-ghost dark:uppercase">
+                                        View Code
+                                    </Link>
+                                </div>
+                            </div>
+                            
+                        }
+                        
                     </label>
             </label>
         </>
